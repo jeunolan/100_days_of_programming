725679d9fc49fead12d0633ad23f0fe2753c90ba
Je Nolan <166306681+jeunolan@users.noreply.github.com>
2024-06-19 21:54:25 +0900

    initial day 9 description and code

---
+++ day_9.py
@@ -0,0 +1,4 @@
+#day9 of 100 days of programming
+#program dictates an auction using dictionaries and array, multiple players will use a blide bid
+#and the program will dictate which bid is the higher once all the bids have been placed
+
