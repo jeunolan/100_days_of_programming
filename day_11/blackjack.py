98dc29d9a79c2d00cfe2924b4c1231143cb4edbe
Je Nolan <166306681+jeunolan@users.noreply.github.com>
2024-07-11 15:05:43 +0900

    Update blackjack.py
    update blackjack.py to include multiple function that shows
    computing scores
    comparing scores
    outputing the winner

--- day_11/blackjack.py
+++ day_11/blackjack.py
@@ -4,55 +4,93 @@
 # 
 
 import random
-
+from art import blackjack
+from replit import clear
 
 #logo of blackjack
 
 
-logo = """
-.------.            _     _            _    _            _    
-|A_  _ |.          | |   | |          | |  (_)          | |   
-|( \/ ).-----.     | |__ | | __ _  ___| | ___  __ _  ___| | __
-| \  /|K /\  |     | '_ \| |/ _` |/ __| |/ / |/ _` |/ __| |/ /
-|  \/ | /  \ |     | |_) | | (_| | (__|   <| | (_| | (__|   < 
-`-----| \  / |     |_.__/|_|\__,_|\___|_|\_\ |\__,_|\___|_|\_\\
-      |  \/ K|                            _/ |                
-      `------'                           |__/           
-"""
 
-#print(logo)
 
 #play blackjackfunction
-def playBlackJack():
-    print(logo)
-    
+def deal_cards():
     cards = [11,2,3,4,5,6,7,8,9,10,10,10,10] #card values of the deck with Ace being 11
+    return random.choice(cards)
+''' Calculcate score of players and dealer'''
+
+def calculateSum(cards):
+    if sum(cards)==21 and len(cards)==2:
+        return 0
+    if 11 in cards and sum(cards)>21:
+        cards.remove(11)
+        cards.append(1)
+    return sum(cards)
+
+def compare_scores(playerScore, dealerScore):
+    if playerScore >21 and dealerScore>21:
+        return ("You went over. You lose. 😤")
     
+    if playerScore == dealerScore:
+        return "Draw 🙃"
+    elif dealerScore == 0:
+        return "Lose, opponent has Blackjack 😱"
+    elif playerScore == 0:
+        return "Win with a Blackjack 😎"
+    elif playerScore >21:
+        return "You went over. You Lose 😭"
+    elif dealerScore > 21:
+        return "Opponent went over. You win 😁"
+    elif playerScore>dealerScore:
+        return "You win 😃"
+    else:
+        return "You lose 😤"
+
+def playBlackJack():
+    print(blackjack)
+    
+       
     #define dealer and the player, the player is dealt the first card, dealer is given the second card
     #player is given the third card, the dealer is given the fourth card ...... at random
 
     #place holders for the player and dealer
     dealer = []
     player = []
+    game_over = False
 
     #select random value from the cards values and place it into the players lists
-    for cardLoop in range(2):
-        randomCard = random.choice(cards)
-        player.append(randomCard)
-        randomCard = random.choice(cards)
-        dealer.append(randomCard)
-    
+    for cardLoop in range(2):   
+        player.append(deal_cards())
+        dealer.append(deal_cards())
+'''    
     print(player)
-    print(dealer)
+    print(f"[{dealer[0]}, X]")
+'''
+    #calculate sums of both the player and the dealer but only show the player sum
+    while not game_over:
+    
+        playerScore = calculateSum(player)
+        dealerScore = calculateSum(dealer)
 
-
-#ask the user if they want to play blackjack and continue the loop until they say no.
-
-play = True #inital variable of true
-while play:
-    wantToPlay = input("Do you want to play a game of blackjack? Type 'y' or 'n' ")
-    if wantToPlay.upper() == 'Y': #if the player wants to play then call play function
-        
-        playBlackJack()
+        print(f"   Player's cards: {player}, Player's score: {playerScore}")
+        print(f"   Dealer's cards: {dealer[0]})")
+    if playerScore == 0 or dealerScore == 0 or playerScore > 21:
+        game_over = True
     else:
-        break #end game
+        dealAnotherCard = input("Type 'y' to get another card, type 'n' to pass: ")
+        if dealAnotherCard.upper() == 'Y':
+            player.append(deal_cards())
+        else:
+            game_over = True
+
+    while dealerScore !=0 and dealerScore <17:
+        dealer.append(deal_cards())
+        dealerScore = calculateSum(dealer)
+
+        print(f" Your final hand: {player}, final score: {playerScore}")
+        print(f" Dealer's final hand:{dealer}, dealer score: {dealerScore}")
+        print(compare(playerScore,dealerScore))
+
+ #inital variable of true
+while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == "y":
+    clear()
+    playBlackJack()
